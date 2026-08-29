e73f4ed8a5b7e2c41b23266de3f996a74d0d499a
rajsutreja <rajsutreja70@gmail.com>
2026-08-29 11:20:31 +0530

    OpenAPI config with security update

--- src/main/java/com/project/JobPortalSystem/Config/OpenApiConfig.java
+++ src/main/java/com/project/JobPortalSystem/Config/OpenApiConfig.java
@@ -1,10 +1,10 @@
 package com.project.JobPortalSystem.Config;
 
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 
 @Configuration
@@ -12,18 +12,9 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .components(new Components()
-                        .addSecuritySchemes(
-                                "bearerAuth",
-                                new SecurityScheme()
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")
-                        ))
-                .addServersItem(
-                        new Server()
-                                .url("https://jobportalsystem-production-4cc4.up.railway.app/jobportal")
-                                .description("Railway server")
-                );
+                .components(new Components().addSecuritySchemes("bearerAuth",
+                        new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer").bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
     }
 }
